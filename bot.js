4e5d692fdba0eac85bf265f21918d0fab16e6ff8
SYSTEMS\SaishD <saish.dharvotkar@healthlink.net>
2017-01-20 09:27:42 +1300

    new bits :)

--- bot.js
+++ bot.js
@@ -8,3 +8,13 @@ var Twit = require("twit");
 var config = require("./config");
 
 var T = new Twit(config);
+
+var p = { q: 'nba',
+          count: 2
+};                                                  //api query to trigger the GET request
+
+T.get('search/tweets', p , datafunc);               //GET request
+
+function datafunc(err, data, response) {
+        console.log(data);
+    }                                               //callback function
