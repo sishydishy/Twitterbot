6698ccc8ccdf1d214dd972b0752586ca0865d5e4
SYSTEMS\SaishD <saish.dharvotkar@healthlink.net>
2017-01-19 15:13:36 +1300

    added bot and config js

---
+++ bot.js
@@ -0,0 +1,10 @@
+/**
+ * Created by SaishD on 1/19/2017.
+ */
+
+console.log("this bot is going to be amazing");
+
+var Twit = require("twit");
+var config = require("./config");
+
+var T = new Twit(config);
